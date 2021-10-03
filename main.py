ae7082880ea6f0bfdd7e01bdda107997e3df8afc
DanielShinoda <danielkhaybulin@gmail.com>
2021-10-04 02:35:33 +0300

    Add DataBase

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 from aiogram import Bot, Dispatcher, types
 from aiogram.dispatcher.filters import Text
 from random import randint
+import requests
 
 BOT_TOKEN = "2032324784:AAGtOWAHaLCnlQHIhwhBLQr4jDKrujOvPI8"
 with open("config.json", "r", encoding='utf_8') as read_file:
@@ -20,6 +21,17 @@ async def main():
         dp = Dispatcher(bot=bot)
 
         async def start_handler(event: types.Message):
+            requests.post('https://faithback.herokuapp.com/api/users/',
+                          json={"login": event.from_user.get_mention(as_html=True)})
+            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            keyboard.add(*data['buttons'])
+            await event.answer(
+                data['hello_phrase'][get_ind(data['hello_phrase'])].format(
+                    event.from_user.get_mention(as_html=True)),
+                parse_mode=types.ParseMode.HTML, reply_markup=keyboard
+            )
+
+        async def continue_handler(event: types.Message):
             keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
             keyboard.add(*data['buttons'])
             await event.answer(
@@ -35,14 +47,16 @@ async def main():
             )
 
         @dp.message_handler(Text(equals=data["buttons"][2]))
-        async def with_puree(message: types.Message):
-            await message.reply("/advice - получить совет\n/start, /restart - начало/перезапуск бота\n")
+        async def help_handler(message: types.Message):
+            await message.reply("/advice - получить совет\n/start - начало бота, инициализация юзера в базе данных\n"
+                                "\n/continue - воскрешение бота после отключения сервера")
 
         @dp.message_handler(Text(equals=data["buttons"][0]))
-        async def with_puree(message: types.Message):
+        async def analyse_handler(message: types.Message):
             await message.reply(data["analyse"][get_ind(data["analyse"])])
 
-        dp.register_message_handler(start_handler, commands={"start", "restart"})
+        dp.register_message_handler(start_handler, commands={"start"})
+        dp.register_message_handler(continue_handler, commands={"continue"})
         dp.register_message_handler(get_mood_handler, commands={"advice"})
         await dp.start_polling()
     finally:
