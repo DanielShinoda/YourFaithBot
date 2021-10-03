4b02cabc1d65101914d0d52cba567b5d5dce383a
DanielShinoda <danielkhaybulin@gmail.com>
2021-10-03 23:52:29 +0300

    Add replies

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
 import asyncio
 import json
 from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher.filters import Text
 from random import randint
+import logging
 
 BOT_TOKEN = "2032324784:AAGtOWAHaLCnlQHIhwhBLQr4jDKrujOvPI8"
 with open("config.json", "r", encoding='utf_8') as read_file:
     data = json.load(read_file)
 
 
-async def start_handler(event: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = [data["analyse"], data["check"], data["help"], data["error"]]
-    keyboard.add(*buttons)
-    await event.answer(
-        data['hello_phrase'][randint(0, len(data['hello_phrase']) - 1)].format(event.from_user.get_mention(as_html=True)),
-        parse_mode=types.ParseMode.HTML, reply_markup=keyboard
-    )
-
-
-async def get_mood_handler(event: types.Message):
-    await event.answer(
-        data['fun_advice'],
-        parse_mode=types.ParseMode.HTML, disable_web_page_preview=True
-    )
-
-
-async def help_handler(event: types.Message):
-    await event.answer(
-        f"/advice - получить совет\n"
-        f"/start, /restart - начало/перезапуск бота\n",
-        parse_mode=types.ParseMode.HTML
-    )
+def get_ind(arr):
+    return randint(0, len(arr) - 1)
 
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
 
     try:
-        dispatcher = Dispatcher(bot=bot)
-        dispatcher.register_message_handler(start_handler, commands={"start", "restart"})
-        dispatcher.register_message_handler(get_mood_handler, commands={"advice"})
-        await dispatcher.start_polling()
+        dp = Dispatcher(bot=bot)
+
+        async def start_handler(event: types.Message):
+            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            keyboard.add(*data['buttons'])
+            await event.answer(
+                data['hello_phrase'][get_ind(data['hello_phrase'])].format(
+                    event.from_user.get_mention(as_html=True)),
+                parse_mode=types.ParseMode.HTML, reply_markup=keyboard
+            )
+
+        async def get_mood_handler(event: types.Message):
+            await event.answer(
+                data['fun_advice'],
+                parse_mode=types.ParseMode.HTML, disable_web_page_preview=True
+            )
+
+        @dp.message_handler(Text(equals=data["buttons"][2]))
+        async def with_puree(message: types.Message):
+            await message.reply("/advice - получить совет\n/start, /restart - начало/перезапуск бота\n")
+
+        @dp.message_handler(Text(equals=data["buttons"][0]))
+        async def with_puree(message: types.Message):
+            await message.reply(data["analyse"][get_ind(data["analyse"])])
+
+        dp.register_message_handler(start_handler, commands={"start", "restart"})
+        dp.register_message_handler(get_mood_handler, commands={"advice"})
+        await dp.start_polling()
     finally:
         await bot.close()
 
