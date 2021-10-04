6db14d173427f8353cc75e0208a23b4d14188292
DanielShinoda <danielkhaybulin@gmail.com>
2021-10-04 04:06:25 +0300

    Add bs4 html user name parser

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import asyncio
 import json
 from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from random import randint
+from aiogram.utils import executor
 import requests
 from bs4 import BeautifulSoup
 
@@ -15,54 +19,72 @@ def get_ind(arr):
     return randint(0, len(arr) - 1)
 
 
-async def main():
-    bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN)
 
-    try:
-        dp = Dispatcher(bot=bot)
 
-        async def start_handler(event: types.Message):
-            soup = BeautifulSoup(event.from_user.get_mention(as_html=True), 'html.parser')
-            user_name = soup.find_all('a')[0].text
-            requests.post('https://faithback.herokuapp.com/api/users/',
-                          json={"login": user_name})
-            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            keyboard.add(*data['buttons'])
-            await event.answer(
-                data['hello_phrase'][0].format(
-                    event.from_user.get_mention(as_html=True)),
-                parse_mode=types.ParseMode.HTML, reply_markup=keyboard
-            )
+class Form(StatesGroup):
+    mood = State()  # Получение настроения
 
-        async def continue_handler(event: types.Message):
-            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            keyboard.add(*data['buttons'])
-            await event.answer(
-                data['hello_phrase'][get_ind(data['hello_phrase'])].format(
-                    event.from_user.get_mention(as_html=True)),
-                parse_mode=types.ParseMode.HTML, reply_markup=keyboard
-            )
 
-        async def get_mood_handler(event: types.Message):
-            await event.answer(
-                data['fun_advice'],
-                parse_mode=types.ParseMode.HTML, disable_web_page_preview=True
-            )
+storage = MemoryStorage()
+dp = Dispatcher(bot=bot, storage=storage)
 
-        @dp.message_handler(Text(equals=data["buttons"][2]))
-        async def help_handler(message: types.Message):
-            await message.reply("/advice - получить совет\n/start - начало бота, инициализация юзера в базе данных\n"
-                                "\n/continue - воскрешение бота после отключения сервера")
 
-        @dp.message_handler(Text(equals=data["buttons"][0]))
-        async def analyse_handler(message: types.Message):
-            await message.reply(data["analyse"][get_ind(data["analyse"])])
+async def start_handler(event: types.Message):
+    soup = BeautifulSoup(event.from_user.get_mention(as_html=True), 'html.parser')
+    user_name = soup.find_all('a')[0].text
+    # Если человек уже есть в базе данных, не нужно добавлять его ещё раз
+    flag = False
+    r = requests.get('https://faithback.herokuapp.com/api/users/')
+    for i in r.json():
+        if i['login'] == user_name:
+            flag = True
+            break
+    # Если человек не найден в базе данных: добавляем его
+    if not flag:
+        requests.post('https://faithback.herokuapp.com/api/users/', json={"login": user_name})
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*data['buttons'])
+    await event.answer(
+        data['hello_phrase'][0].format(
+            event.from_user.get_mention(as_html=True)),
+        parse_mode=types.ParseMode.HTML, reply_markup=keyboard
+    )
 
-        dp.register_message_handler(start_handler, commands={"start"})
-        dp.register_message_handler(continue_handler, commands={"continue"})
-        dp.register_message_handler(get_mood_handler, commands={"advice"})
-        await dp.start_polling()
-    finally:
-        await bot.close()
 
-asyncio.run(main())
+# При нажатии на кнопку Помощь
+@dp.message_handler(Text(equals=data["buttons"][2]))
+async def help_handler(message: types.Message):
+    await message.reply("/start - начало бота, инициализация юзера в базе данных")
+
+
+# При нажатии на кнопку Анализ
+@dp.message_handler(Text(equals=data["buttons"][0]))
+async def analyse_handler(message: types.Message):
+    await Form.mood.set()
+    await message.reply(data["analyse"][get_ind(data["analyse"])])
+
+
+# Запись в базу данных настроения
+@dp.message_handler(state=Form.mood)
+async def process_name(event: types.Message, state: FSMContext):
+    soup = BeautifulSoup(event.from_user.get_mention(as_html=True), 'html.parser')
+    user_name = soup.find_all('a')[0].text
+    r = requests.get('https://faithback.herokuapp.com/api/users/')
+    user = -1
+    for i in r.json():
+        if i['login'] == user_name:
+            user = i['id']
+            break
+    if user > 0:
+        requests.post('https://faithback.herokuapp.com/api/mood/',
+                      json={"mood": event.text, "user": user})
+
+    await state.finish()
+    await event.reply(data["bye_phrase"][get_ind(data["analyse"])])
+
+dp.register_message_handler(start_handler, commands={"start"})
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
